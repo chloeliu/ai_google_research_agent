06ace577ff86cc0d9ceceb90d245272182ae246c
Chloe Liu <chloeliuyy@gmail.com>
2023-10-14 11:21:52 -0700

    tune the prompt for report writer

--- google_researcher.py
+++ google_researcher.py
@@ -359,7 +359,7 @@ def main():
         prompt_template = """
         Base on the objective for this research: "{objective}",
         - write the following sections of a reserach report: 
-        -- "Executive Summary (bullet points finding highlights that answer user request, less than 3, be short and cociese, and to the point)
+        -- "Executive Summary (bullet points of conclusiosn and key points from findings that answer user request, less than 3, be short and cociese, and to the point)
         -- "Analysis and Interpretation" (observations and patterns you've noticed from the information gathered so far and it is highly relevant to the user request"
         - link in-text url and references links throughout the section
         - Write the reports using markdown style categorizing information so that it is easy for the user to read.
@@ -376,7 +376,7 @@ def main():
         prompt_template = """
         Base on the objective for this research: "{objective}",
         - write the following sections as part of an overall report: 
-        --Detail findings (reference url and snippet of reference articles in reporting format break down in sub-sections if needed), 
+        -- Detail findings (reference url and summaries or snippet of reference articles in reporting format break down in sub-sections if needed), 
         -- Key Resources (list of referenced resources by category) 
         -- Method (list the research steps you've taken for this report in short bullet point)",        
         - link text to relevant url and references links throughout the section
